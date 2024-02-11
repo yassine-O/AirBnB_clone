fad8106dd5a1373e339e384f6518e2a49eb25556
Yassine <yassine@gmail.com>
2024-02-11 14:10:01 -0800

    unittest

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -19,15 +19,6 @@ class TestBaseModel(unittest.TestCase):
         """ tear down class """
         del cls.base
 
-    def test_init(self):
-        """ test __init__ method """
-        self.assertTrue(isinstance(self.base, BaseModel))
-
-    def test_save(self):
-        """ test save method """
-        self.base.save()
-        self.assertNotEqual(self.base.created_at, self.base.updated_at)
-
     def test_to_dict(self):
         """ test to_dict method """
         base_dict = self.base.to_dict()
