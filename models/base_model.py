9413598e8a19da9cee04bd1f56b77aef080ca4f4
Yassine <yassine@gmail.com>
2024-02-11 13:11:46 -0800

    BaseModel : unittest

--- models/base_model.py
+++ models/base_model.py
@@ -17,7 +17,8 @@ class BaseModel():
 
     def __str__(self):
         """ [<class name>] (<self.id>) <self.__dict__> """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return ("[{}] ({}) {}"
+                .format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
         """ updates the public instance attribute updated_at """
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" Unittest for BaseModel class """
+
+import unittest
+
+
+class TestBaseModel(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
