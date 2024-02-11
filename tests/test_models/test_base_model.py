07f49f3c118f592d01b4034d0a5fba216c39be73
Yassine <yassine@gmail.com>
2024-02-11 14:07:28 -0800

    Base Model unit tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -7,23 +7,29 @@ from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
+    """ BaseModel unit test """
 
     @classmethod
     def setUpClass(cls):
+        """ set up class """
         cls.base = BaseModel()
 
     @classmethod
     def tearDownClass(cls):
+        """ tear down class """
         del cls.base
 
     def test_init(self):
+        """ test __init__ method """
         self.assertTrue(isinstance(self.base, BaseModel))
 
     def test_save(self):
+        """ test save method """
         self.base.save()
         self.assertNotEqual(self.base.created_at, self.base.updated_at)
 
     def test_to_dict(self):
+        """ test to_dict method """
         base_dict = self.base.to_dict()
         self.assertEqual(base_dict['__class__'], 'BaseModel')
         self.assertIsInstance(base_dict['created_at'], str)
